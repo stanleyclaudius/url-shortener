75a3a7116b7429c383a528388a445b5e661d1b4c
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-06-29 19:40:11 +0700

    Move illustration image to separate component from home page

---
+++ client/src/components/general/Illustration.tsx
@@ -0,0 +1,11 @@
+const Illustration = () => {
+  return (
+    <>
+      <img src='/images/right-illustration.png' alt='URL Shortify' width={500} className='md:block hidden absolute -top-1 right-0 -z-[999]' />
+      <img src='/images/left-illustration.png' alt='URL Shortify' className='md:block hidden absolute bottom-0 left-0 -z-[999]' />
+      <img src='/images/bottom-illustration.png' alt='URL Shortify' className='md:block hidden absolute -bottom-36 -right-32 -z-[999]' />
+    </>
+  )
+}
+
+export default Illustration
