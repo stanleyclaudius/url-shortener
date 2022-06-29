d6488ef11607057cf367e6f6b4b50ddfdc43532b
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-06-29 09:53:09 +0700

    Fix responsiveness issue at home page

--- client/src/components/general/Navbar.tsx
+++ client/src/components/general/Navbar.tsx
@@ -24,13 +24,14 @@ const Navbar = () => {
       <Link to='/'>
         <h1 className='text-xl font-medium'>URL Shortify</h1>
       </Link>
-      <div className='md:hidden block' onClick={() => setOpenSidebar(true)}>
+      <div className='md:hidden block cursor-pointer' onClick={() => setOpenSidebar(true)}>
         <GiHamburgerMenu />
       </div>
       <div ref={sidebarRef} className={`md:p-0 p-6 flex md:flex-row flex-col md:items-center md:gap-10 gap-6 text-sm md:static fixed top-0 ${openSidebar ? 'right-0' : '-right-[2000px]'} md:h-auto h-screen md:shadow-none shadow-xl md:border-none border border-gray-200 md:w-auto w-[200px] md:bg-transparent bg-white transition-all`}>
-        <div className='md:hidden block' onClick={() => setOpenSidebar(false)}>
-          <AiOutlineClose />
+        <div className='md:hidden block cursor-pointer' onClick={() => setOpenSidebar(false)}>
+          <AiOutlineClose className='float-right' />
         </div>
+        <div className='clear-both' />
         <Link to='/login' className='w-fit'>Sign In</Link>
         <Link to='/register' className='bg-primary hover:bg-primaryHover text-white rounded-md px-3 py-2 transition-all w-fit'>Sign Up</Link>
       </div>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -13,15 +13,17 @@ const Home = () => {
       <Head title='Home' />
       <div className='flex flex-col h-screen'>
         <Navbar />
-        <div className='flex flex-col justify-center flex-1'>
+        <div className='flex flex-col justify-center flex-1 px-6'>
           <div className='text-center'>
             <h1 className='text-3xl font-semibold text-primaryHover mb-4'>Create Short Links!</h1>
             <p className='text-sm text-gray-700 w-full max-w-[600px] leading-7 m-auto'>URL Shortify is a custom link personalization tool that enables you to target, engage, and drive more customers. Get started for free.</p>
             <div className='m-auto bg-white shadow-xl w-full max-w-[700px] rounded-lg border border-gray-100 p-7 mt-7'>
-              <div className='bg-gray-100 p-4 rounded-md flex items-center gap-4'>
-                <BsLink45Deg className='text-3xl text-gray-500' />
-                <input type='text' value={url} onChange={e => setUrl(e.target.value)} placeholder='Paste a link to shorten it' className='w-full bg-transparent text-sm outline-0 text-gray-500' />
-                <button onClick={() => setOpenModal(true)} className='text-sm bg-primary hover:bg-primaryHover transition-all rounded-md outline-0 shadow-xl text-white px-4 py-3'>Shorten</button>
+              <div className='md:bg-gray-100 md:p-4 rounded-md flex md:flex-row flex-col items-center gap-4'>
+                <div className='md:bg-transparent md:p-0 p-4 md:rounded-none rounded-md bg-gray-100 flex items-center gap-4 flex-1 w-full'>
+                  <BsLink45Deg className='text-3xl text-gray-500' />
+                  <input type='text' value={url || ''} onChange={e => setUrl(e.target.value)} placeholder='Paste a link to shorten it' className='w-full bg-transparent text-sm outline-0 text-gray-500' />
+                </div>
+                <button onClick={() => setOpenModal(true)} className='mt-2 md:mt-0 text-sm bg-primary hover:bg-primaryHover transition-all rounded-md outline-0 shadow-xl text-white px-4 py-3'>Shorten</button>
               </div>
             </div>
             <p className='text-xs text-gray-400 mt-7'>Use it, it's Free - Fast - Secure - Long Term Link</p>
