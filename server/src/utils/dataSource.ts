c31ef445e62dac313f564b4f181fe952363704c2
Stanley Claudius <stanleyclaudiuss@gmail.com>
2022-07-02 11:34:45 +0700

    Dockerize server application

--- server/src/utils/dataSource.ts
+++ server/src/utils/dataSource.ts
@@ -19,6 +19,7 @@ if (isProduction) {
 } else {
   obj = {
     type: 'postgres',
+    host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
